f07f46dd15f0ba8098744d5892d626d51179ca29
Venkatesh Naidu <venkynaidu1997@gmail.com>
2018-09-12 19:57:21 +0530

    join

--- feedbacks/urls.py
+++ feedbacks/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls import url
 
 urlpatterns = [
 	path('', views.course, name='course'),
-	url(r'^GET', views.feed, name='feed')
+	url(r'^GET', views.feed, name='feed'),
 ] 
--- wizkids/views.py
+++ wizkids/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
+from feedbacks.models import feedback
+from django.contrib.auth.models import User
+from django.contrib import auth
+from accounts.models import account
+
 
 def home(request):
-	return render(request, 'home.html')
+	feeds = feedback.objects
+	'''acc = account.objects
+				f = list(feeds.all())
+				a = list(acc.all())
+				for i in range(0,len(f)):
+					b = account.objects.filter(userid__exact = f[i].userid)
+					print(b)'''
+	d = {}
+	for feed in feeds.all():
+		o = account.objects.get(userid=feed.userid)
+		if feed.userid not in d:
+			d[feed.userid] = o.username
+	print(d[2])
+	b = {}
+	a = 0
+	return render(request, 'home.html', {'feeds' : feeds, 'd' : d, 'b' : a})
