eccaa1752a3d306af23e4e8a3bf65f47ebd4469e
Mrinal <dmrinal626@gmail.com>
2018-10-06 11:08:00 +0530

    Auto stash before merge of "master" and "origin/master"

--- creator/views.py
+++ creator/views.py
@@ -4,9 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 from .models import course,topic,content
 
-CID = None
 TOPICS = None
-
+CID = None
+import json
+from django.core.files.storage import FileSystemStorage
 def creator(request):
     # displaying the course page!!!
     c = course.objects
@@ -26,7 +27,7 @@ def topics(request):
 def resource(request):
     ids = request.GET['foreignKey']
     con = content.objects.filter(tid=ids)
-    return render(request,'creator.html',{'keys':con,'chk':2})
+    return render(request,'creator.html',{'keys':con,'chk':2,'topicID':ids})
 
 def addcourse(request):
     temp = request.POST['pickachu']
@@ -41,6 +42,30 @@ def addcourse(request):
         details.save()
         return creator(request)
 
+
+def addRes(request):
+    temp = request.POST['resId']
+    print(temp)
+    naam = topic.objects.filter(ids=request.POST['Fkey'])
+    cons = content.objects.filter(tid=request.POST['Fkey'])
+    if temp=='1':
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        print(uploaded_file_url)
+        data ={"link":uploaded_file_url}
+        dataJson = json.dumps(data)
+        details = content(tid=naam[0],code= request.POST['rSelected'],data= dataJson,oid=request.POST['orderid'])
+    else:
+        # all about text and questions
+        ques = request.POST['title']
+        ans = request.POST['summary']
+        data = {ques:ans}
+        dataJson = json.dumps(data)
+        details = content(tid=naam[0],code= request.POST['rSelected'],data=dataJson,oid=request.POST['orderid'])
+    details.save()
+    return render(request,'creator.html',{'keys':cons,'chk':2,'topicID':request.POST['Fkey']}) 
 def reorder(request):
     global CID,TOPICS
     tops = TOPICS
